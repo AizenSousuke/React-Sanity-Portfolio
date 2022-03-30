9413ae47864cc0fbc2e5491534c3b095c52c2004
AizenSousuke <puteranikdimas@yahoo.com.sg>
2022-03-30 20:40:17 +0800

    Added MotionWrap component

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -4,7 +4,7 @@ import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 import ReactTooltip from "react-tooltip";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 const Skills = () => {
@@ -27,7 +27,7 @@ const Skills = () => {
 			<h2 className="head-text">Skills & Experience</h2>
 			<div className="app__skills-container">
 				<motion.div className="app__skills-list">
-					{skills.map((skill, index) => (
+					{skills.map((skill) => (
 						<motion.div
 							whileInView={{ opacity: [0, 1] }}
 							transition={{ duration: 0.5 }}
@@ -47,9 +47,54 @@ const Skills = () => {
 						</motion.div>
 					))}
 				</motion.div>
+				<motion.div className="app__skills-exp">
+					{experiences.map((experience) => (
+						<motion.div
+							className="app__skills-exp-item"
+							key={experience.year}
+						>
+							<div className="app__skills-exp-year">
+								<p className="bold-text">{experience.year}</p>
+							</div>
+							<motion.div className="app__skills-exp-works">
+								{experience.works.map((work) => (
+									<>
+										<motion.div
+											whileInView={{ opacity: [0, 1] }}
+											transition={{ duration: 0.5 }}
+											className="app__skills-exp-work"
+											data-tip
+											data-for={work.desc}
+											key={work.name}
+										>
+											<h4 className="bold-text">
+												{work.name}
+											</h4>
+											<p className="p-text">
+												{work.company}
+											</p>
+										</motion.div>
+										<ReactTooltip
+											id={work.desc}
+											effect="solid"
+											arrowColor="#fff"
+											className="skills-tooltip"
+										>
+											{work.desc}
+										</ReactTooltip>
+									</>
+								))}
+							</motion.div>
+						</motion.div>
+					))}
+				</motion.div>
 			</div>
 		</>
 	);
 };
 
-export default Skills;
+export default AppWrap(
+	MotionWrap(Skills, "app__skills"),
+	"skills",
+	"app__whitebg"
+);
